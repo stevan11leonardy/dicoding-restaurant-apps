27919dccd820f2a2ab0c35b4929dbaca87128354
Stevan Leonardy <stevan.leonardy@pmberjaya.com>
2021-05-06 09:36:32 +0700

    update

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -4,8 +4,7 @@ const CONFIG = {
   BASE_IMAGE_URL: 'https://restaurant-api.dicoding.dev/images/',
   DATABASE_NAME: 'restaurant-catalogue-database',
   DATABASE_VERSION: 1,
-  // CACHE_NAME: 'RestaurantCatalogue-V1',
-  CACHE_NAME: new Date().toISOString(),
+  CACHE_NAME: 'RestaurantCatalogue-V1',
 };
 
 export default CONFIG;
